a25c8aa73bf51230f80c7002acc58466a02d0461
Sonu Rathod <thesonurathod@gmail.com>
2019-12-16 03:02:26 -0800

    inital commit

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+n = int(input('enter the number'))
+temp = n
+reverseNo = 0
+while temp != 0:
+    reverseNo = (reverseNo * 10) + (temp % 10)
+    temp = temp // 10
+if reverseNo == number:
+    print('yes :), PALINDROM')
+else:
+    print('no :ssss(')
---
+++ prime_number_python/prime_number.py
@@ -0,0 +1,17 @@
+a=int(input('please enter the last number of your range ( : ? ) : '))
+b=1
+while b < 2 :
+   b=int(input('please enter the first number of your range ( ? : ) : '))
+   if b<2:
+      print(f"The first number should be at least 2, Please select agian the first number")
+
+    
+prime_number = []
+for i in range(b,a+1):
+    prime_number.append(i)
+    for j in range(2,i):
+        if i%j == 0:
+            prime_number.remove(i)  
+            break
+
+print('prime numeber is :',prime_number)
